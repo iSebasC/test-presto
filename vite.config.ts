ce2097d2d3e1f438d2970f5a70f8fbf9e6a1656a
iSebasC <sebas.cabrera.456@gmail.com>
2025-05-18 19:30:52 -0500

    crear proyecto y detalle

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,24 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
+import { resolve, dirname } from 'path'
+import { fileURLToPath } from 'url'
+
+// Definir __dirname manualmente
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+
 export default defineConfig({
   plugins: [
     tailwindcss(),
   ],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        proyecto: resolve(__dirname, 'proyecto.html'),
+        crear_proyecto: resolve(__dirname, 'crear_proyecto.html'),
+        proyecto_detalle: resolve(__dirname, 'proyecto_detalle.html'),
+      },
+    },
+  },
 })
